d921a45679b89562651825a93b0487c9602da889
dalramjwi <dalramjwi@gmail.com>
2024-06-22 14:29:54 +0900

    noEmitonError use

--- first.js
+++ first.js
@@ -1,1 +1,6 @@
 console.log("first");
+// 아래는 실무 수준에서 범용적으로 쓰이는 환영 함수입니다
+function greet(person, date) {
+  console.log("Hello ".concat(person, ", today is ").concat(date, "!"));
+}
+greet("Brendan");
--- first.ts
+++ first.ts
@@ -1,1 +1,7 @@
 console.log("first");
+// 아래는 실무 수준에서 범용적으로 쓰이는 환영 함수입니다
+function greet(person, date) {
+  console.log(`Hello ${person}, today is ${date}!`);
+}
+
+greet("Brendan");
