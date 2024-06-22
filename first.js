c1d07e5c102c8a9f62b94c60d4709f6c68eb6d1a
dalramjwi <dalramjwi@gmail.com>
2024-06-22 20:15:39 +0900

    back to commet change

--- first.js
+++ first.js
@@ -1,7 +1,6 @@
 console.log("first");
 // 아래는 실무 수준에서 범용적으로 쓰이는 환영 함수입니다
-function greet(person, date) {
-    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
-}
-greet("Maddison", new Date());
-var msg = "hello there!";
+// function greet(person, date) {
+//   console.log("Hello ".concat(person, ", today is ").concat(date, "!"));
+// }
+// greet("Brendan");
